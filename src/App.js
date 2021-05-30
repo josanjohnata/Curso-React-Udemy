c030e0aa7574fc5d7ebd75d7149ebd633ec800ef
josanjohnata <64118585+josanjohnata@users.noreply.github.com>
2021-05-30 00:24:23 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Navbar from "./components/NavBar/Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="App">
+      <span className="navbar">LIST TO DO</span>
+    </nav>
+  );
+}
