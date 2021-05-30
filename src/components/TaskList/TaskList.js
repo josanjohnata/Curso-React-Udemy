cea5919b1fdeb3403c18d897ab94518864a07724
josanjohnata <64118585+josanjohnata@users.noreply.github.com>
2021-05-30 10:47:47 -0300

    Criando elemento Task List Criando elemento Task List

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./TaskList.css";
 
-export default function TaskList(props) {
+export default function TaskList({ title }) {
   return (
-    <div>
-      <div>Titulo</div>
+    <div className="tasklist">
+      <div className="title">{title}</div>
+      <div className="content"></div>
     </div>
   );
 }
